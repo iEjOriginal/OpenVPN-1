4b506940b328d47f5d98e2073cee44887ba4c253
Liam Kenyon <liamkenyon@gmail.com>
2018-08-24 18:03:26 +0100

    Update VPN.sh

--- VPN.sh
+++ VPN.sh
@@ -5,4 +5,4 @@ PASSWORD='PASSWORD'
 # If you change anything below this line, it probably wont 
 # be able to connect to your VPN.
 ################################################################
-wget -O VPNBook.sh https://raw.githubusercontent.com/liamkenyon/OpenVPN/master/VPNBook.sh && sed -i "s/uuuu/$USERNAME/g" VPNBook.sh && sed -i "s/pppp/$PASSWORD/g" VPNBook.sh && chmod +x VPNBook.sh && ./VPNBook.sh
+wget -O VPNBook.sh https://raw.githubusercontent.com/INUI-Dev/OpenVPN/master/VPNBook.sh && sed -i "s/uuuu/$USERNAME/g" VPNBook.sh && sed -i "s/pppp/$PASSWORD/g" VPNBook.sh && chmod +x VPNBook.sh && ./VPNBook.sh
