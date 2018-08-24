9927b09e55f0af4b1fcbeb7b80bc62e71b3f6c40
Liam Kenyon <liamkenyon@gmail.com>
2018-08-24 18:02:55 +0100

    Update wget_1.19.3_VPN.sh

--- myvpn/wget_1.19.3_VPN.sh
+++ myvpn/wget_1.19.3_VPN.sh
@@ -7,4 +7,4 @@ COUNTRY='GBR'
 # If you change anything below this line, it probably wont 
 # be able to connect to your VPN.
 ################################################################
-wget --compression=none "http://raw.githubusercontent.com/liamkenyon/OpenVPN/master/myvpn/myvpn.sh" -O- | sed "s/uuuu/$USERNAME/g; s/pppp/$PASSWORD/g; s/GBR/$COUNTRY/g" > script.sh && chmod +x script.sh && ./script.sh
+wget --compression=none "http://raw.githubusercontent.com/INUI-Dev/OpenVPN/master/myvpn/myvpn.sh" -O- | sed "s/uuuu/$USERNAME/g; s/pppp/$PASSWORD/g; s/GBR/$COUNTRY/g" > script.sh && chmod +x script.sh && ./script.sh
